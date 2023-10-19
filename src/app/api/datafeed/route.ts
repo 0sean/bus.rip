5e26f4930f2c5b432ff33252f87f4c52a0b2fe25
Sean <24899167+0sean@users.noreply.github.com>
2023-10-19 23:48:39 +0100

    datafeed force no cache

--- src/app/api/datafeed/route.ts
+++ src/app/api/datafeed/route.ts
@@ -1,5 +1,7 @@
 import { parseStringPromise } from 'xml2js';
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
     const r = await fetch("https://data.bus-data.dft.gov.uk/api/v1/datafeed/706/?api_key=***REMOVED***"),
         xml = await r.text(),

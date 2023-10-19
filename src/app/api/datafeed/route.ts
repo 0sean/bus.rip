992a492fc72b7de7a7a1b7f82a7a07478fa87970
Sean <24899167+0sean@users.noreply.github.com>
2023-10-19 23:33:01 +0100

    v1

---
+++ src/app/api/datafeed/route.ts
@@ -0,0 +1,9 @@
+import { parseStringPromise } from 'xml2js';
+
+export async function GET(request: Request) {
+    const r = await fetch("https://data.bus-data.dft.gov.uk/api/v1/datafeed/706/?api_key=***REMOVED***"),
+        xml = await r.text(),
+        json = await parseStringPromise(xml);
+
+    return Response.json(json);
+}

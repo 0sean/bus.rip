679c71aba0a59841dae4811451c9863536e524c7
Sean <24899167+0sean@users.noreply.github.com>
2023-10-20 22:48:41 +0100

    Custom markers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import { useEffect, useRef, useState } from "react";
 import useSWR from "swr";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { Inter } from "next/font/google";
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function MapPage() {
   const mapContainer = useRef(null),
@@ -56,12 +59,31 @@ export default function MapPage() {
     const newMarkers: maplibregl.Marker[] = [];
     data.Siri.ServiceDelivery[0].VehicleMonitoringDelivery[0].VehicleActivity.forEach(
       (va: Record<string, unknown>) => {
+        if(new Date((va.ValidUntilTime as string[])[0] + "Z") < new Date() || (Number(new Date()) - Number(new Date((va.RecordedAtTime as string[])[0]))) >= 900000) return; // if no longer valid or more than 15 minutes old
+        const el = document.createElement("div");
+        el.className = "marker";
+        el.style.backgroundImage = "url('/marker.svg')";
+        el.style.width = "20px";
+        el.style.height = "48px";
+        const label = document.createElement("div");
+        label.textContent = (va.MonitoredVehicleJourney as any)[0].PublishedLineName;
+        label.style.width = "28px";
+        label.style.height = "28px";
+        label.style.textAlign = "center";
+        label.style.fontWeight = "bold";
+        label.style.backgroundColor = "#4264fb";
+        label.style.padding = "4px";
+        label.style.borderRadius = "100%";
+        label.style.marginTop = "-12px";
+        label.style.marginLeft = "-4px";
+        label.style.fontFamily = inter.style.fontFamily
+        el.appendChild(label);
         const popup = new maplibregl.Popup({ offset: 25 }).setText(
             `${(va.MonitoredVehicleJourney as any)[0].PublishedLineName} - ${
               (va.MonitoredVehicleJourney as any)[0].DestinationName
-            }`
+            } - Last updated: ${new Date((va.RecordedAtTime as string[])[0]).toLocaleString()}`
           ),
-          marker = new maplibregl.Marker()
+          marker = new maplibregl.Marker({element: el})
             .setLngLat([
               Number(
                 (va.MonitoredVehicleJourney as any)[0].VehicleLocation[0]
@@ -74,7 +96,7 @@ export default function MapPage() {
             ])
             .setPopup(popup)
             .addTo(map.current as maplibregl.Map);
-        markers.push(marker);
+        newMarkers.push(marker);
       }
     );
     setMarkers(newMarkers);

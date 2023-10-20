6b2d0c7ce11f46c483a8955154bccd33d723ba58
Sean <24899167+0sean@users.noreply.github.com>
2023-10-20 21:38:29 +0100

    BODS API caching

--- src/app/api/datafeed/route.ts
+++ src/app/api/datafeed/route.ts
@@ -1,9 +1,9 @@
 import { parseStringPromise } from 'xml2js';
 
-export const dynamic = "force-dynamic";
-
 export async function GET(request: Request) {
-    const r = await fetch("https://data.bus-data.dft.gov.uk/api/v1/datafeed/706/?api_key=***REMOVED***"),
+    const r = await fetch("https://data.bus-data.dft.gov.uk/api/v1/datafeed/706/?api_key=***REMOVED***", {
+        next: { revalidate: 10 }
+    }),
         xml = await r.text(),
         json = await parseStringPromise(xml);
 

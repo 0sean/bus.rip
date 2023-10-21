1ff0038a7f6c24f736b058fec70e163b893231df
Sean <24899167+0sean@users.noreply.github.com>
2023-10-21 18:09:20 +0100

    h-full instead of h-screen (mobile fix?)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-zinc-950 dark`}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} bg-zinc-950 dark h-full`}>{children}</body>
     </html>
   )
 }

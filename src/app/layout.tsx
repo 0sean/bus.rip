177bba0cf7ee20970c9545e56a2b52873b31b80d
Sean <24899167+0sean@users.noreply.github.com>
2024-03-28 19:14:25 +0000

    Migrate to new Vercel Speed Insights package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-zinc-950 dark h-full`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
